0331bd20916815c65722d76b8f6c5e8b5400edae
Cedrick Therrien <therrien.cedrick@gmail.com>
2023-12-16 15:34:53 -0500

    feat: implemented InputError component

---
+++ src/lib/components/InputError.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let errors: string[] | undefined
+</script>
+
+ {#if (errors?.length)}
+    {#each errors as error }
+        <p class="text-red-500">{error}</p>
+    {/each}
+ {/if}
